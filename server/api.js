7a0b767430d32c7fd1d6670a4e3baf9dfe826430
mariaburmeister <mariaburmeister@gmail.com>
2021-01-11 20:38:50 +0100

    Set up body-parsing middleware with body-parser.

--- server/api.js
+++ server/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
+const bodyParser = require('body-parser');
 
+apiRouter.use(bodyParser.json());
 
 
 module.exports = apiRouter;
