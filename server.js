a1d63d070ceb878e803c99f371832001a7cb7b40
mariaburmeister <mariaburmeister@gmail.com>
2021-01-12 12:37:33 +0100

    Change body-parser and cors setups to server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// const apiRouter = require('./server/api');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.use('/api', apiRouter);
 
 module.exports = app;
 
--- server/api.js
+++ server/api.js
@@ -1,10 +1,4 @@
 const express = require('express');
 const apiRouter = express.Router();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-apiRouter.use(bodyParser.json());
-apiRouter.use(cors());
-
 
 module.exports = apiRouter;
