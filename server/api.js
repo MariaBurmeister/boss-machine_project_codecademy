7c050068d0bb666744c4ffb2da302435535a34aa
mariaburmeister <mariaburmeister@gmail.com>
2021-01-12 11:39:02 +0100

    Set up CORS middleware with cors - default setting

--- server/api.js
+++ server/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 apiRouter.use(bodyParser.json());
+apiRouter.use(cors());
 
 
 module.exports = apiRouter;
