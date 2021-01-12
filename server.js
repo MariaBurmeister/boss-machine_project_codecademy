fbd8f9ae5693f6588357b8d5a5b3618bf5ea1e6b
mariaburmeister <mariaburmeister@gmail.com>
2021-01-12 12:38:09 +0100

    set up apiRouter to /api at server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// const apiRouter = require('./server/api');
+const apiRouter = require('./server/api');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.use('/api', apiRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
 
